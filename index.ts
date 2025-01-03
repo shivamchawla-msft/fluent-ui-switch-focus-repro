b3c18ebe14dce6a231fd7dbbe79f6518466de4d7
shivamchawla-msft <116705888+shivamchawla-msft@users.noreply.github.com>
2025-01-03 11:54:42 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,12 +1,28 @@
+import '@fluentui/web-components/label.js';
+import '@fluentui/web-components/switch.js';
 
-import '@fluentui/web-components/radio-group.js';
-import '@fluentui/web-components/radio.js';
-import '@fluentui/web-components/field.js';
+import { customElement, FASTElement, html } from '@microsoft/fast-element';
 
-import { setTheme } from '@fluentui/web-components';
 import { webLightTheme } from '@fluentui/tokens';
+import { setTheme } from '@fluentui/web-components';
 setTheme(webLightTheme);
 
-document.querySelector('fluent-radio-group')!.addEventListener('change', (e) => {
-  console.log(e)
+const template = html<MyComponent>`
+  <fluent-field label-position="before">
+    <fluent-label slot="label">Switch label </fluent-label>
+    <fluent-switch
+      @click=${async (x) => {
+        console.log('switch click');
+      }}
+      name="switch"
+      slot="input"
+    >
+    </fluent-switch>
+  </fluent-field>
+`;
+
+@customElement({
+  name: 'my-comp',
+  template: template,
 })
+export class MyComponent extends FASTElement {}
