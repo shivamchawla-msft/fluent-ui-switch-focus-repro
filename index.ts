b532452118309550b54a1f0af304431146c53952
Shivam Chawla <shchaw@microsoft.com>
2025-06-30 17:27:59 +0530

    something

--- index.ts
+++ index.ts
@@ -1,28 +1,41 @@
-import '@fluentui/web-components/label.js';
-import '@fluentui/web-components/switch.js';
+import { customElement, FASTElement, html, ref } from '@microsoft/fast-element';
 
-import { customElement, FASTElement, html } from '@microsoft/fast-element';
-
+import '@fluentui/web-components/dialog-body.js';
+import '@fluentui/web-components/button.js';
+import { ButtonAppearance, ButtonSize } from '@fluentui/web-components/button/index.js';
+import '@fluentui/web-components/dialog.js';
 import { webLightTheme } from '@fluentui/tokens';
+import type { Dialog } from '@fluentui/web-components/dialog/index.js';
 import { setTheme } from '@fluentui/web-components';
 setTheme(webLightTheme);
 
 const template = html<MyComponent>`
-  <fluent-field label-position="before">
-    <fluent-label slot="label">Switch label </fluent-label>
-    <fluent-switch
-      @click=${async (x) => {
-        console.log('switch click');
-      }}
-      name="switch"
-      slot="input"
-    >
-    </fluent-switch>
-  </fluent-field>
+  <fluent-button
+    @click=${(x) => x.confirmationDialog?.show()}
+    appearance=${ButtonAppearance.primary}
+    size=${ButtonSize.large}
+  >
+    ${(x) => 'Show dialog'}
+  </fluent-button>
+  <fluent-dialog ${ref('confirmationDialog')} type="modal">
+    <fluent-dialog-body>
+      <p>${(x) => 'dialog body text'}</p>
+      <div slot="title">${(x) => 'dialog title'}</div>
+      <div slot="title-action">
+        <fluent-button icon-only size=${ButtonSize.small}></fluent-button>
+      </div>
+      <div slot="action" style="display: flex; justify-content: flex-end; gap: 8px;">
+        <fluent-button appearance=${ButtonAppearance.primary}> ${(x) => 'Primary button'} </fluent-button>
+        <fluent-button> "cancel" </fluent-button>
+      </div>
+    </fluent-dialog-body>
+  </fluent-dialog>
 `;
 
 @customElement({
   name: 'my-comp',
   template: template,
 })
-export class MyComponent extends FASTElement {}
+export class MyComponent extends FASTElement {
+  confirmationDialog: Dialog | null = null;
+}
